531841e8ea4d5d347906f272b3b179a01b9a0229
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-04-16 14:01:08 -0300

    circular
    layout

--- app/src/main/java/ramos/adriano/raul/circularpeabiru/MainActivity.kt
+++ app/src/main/java/ramos/adriano/raul/circularpeabiru/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val valorPassagem = 3.7
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
