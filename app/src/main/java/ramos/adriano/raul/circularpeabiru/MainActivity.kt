9f7e920598fa6040aef6aa18a8cd1818bbd29592
DESKTOP-Q3G8KM1\Raul <raul.adriano.ramos@gmail.com>
2018-04-23 20:57:23 -0300

    Circular
    Fix

--- app/src/main/java/ramos/adriano/raul/circularpeabiru/MainActivity.kt
+++ app/src/main/java/ramos/adriano/raul/circularpeabiru/MainActivity.kt
@@ -1,15 +1,27 @@
 package ramos.adriano.raul.circularpeabiru
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val valorPassagem = 3.7
+    val valorPassagem = 3.70
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_valor_passagem.text = "R$ " + valorPassagem+"0"
+
+        SaS.setOnClickListener({
+            var activityDestino = SaSActivity::class.java
+
+            var intent = Intent(this,activityDestino)
+            startActivity(intent)
+
+        })
     }
 }
